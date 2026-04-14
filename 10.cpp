8cefac446633b2caeda698d9c5927267ee7fa0ac
Aniket Mule <muleaniket958@gmail.com>
2026-04-14 12:19:21 +0530

    Add a simple C++ program to greet a user

---
+++ 10.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string name;
+
+    cout << "Enter the name: ";
+    cin >> name;
+
+    cout << "Hello " << name << endl;
+
+    return 0;
+}
